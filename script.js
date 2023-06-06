5b3077354a510b139502e00f79a977a3c9ca0d76
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-06-06 10:28:03 -0400

    Add addElement function within screenController module

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const gameController = ((
         { name: playerOneName, mark: 1 },
         { name: playerTwoName, mark: 2 },
     ];
-
+    const board = gameBoard;
     // Switch Player Logic
     let activePlayer = players[0];
 
@@ -60,5 +60,52 @@ const gameController = ((
     const getPlayer = () => activePlayer;
     // Play Round logic
 
-    return { getPlayer, switchActivePlayer };
+    return { getPlayer, switchActivePlayer, ...board };
+    // return { getPlayer, switchActivePlayer, getBoard: board.getBoard, printBoard: board.printBoard};
+})();
+
+const screenController = (() => {
+    const game = gameController;
+
+    // Select div for player and gameboard
+    const playerTurnDiv = document.querySelector('.playerTurn');
+    const gameBoardDiv = document.querySelector('.gameBoard');
+
+    // Function for adding elements
+    const addElement = (target, element, value) => {
+        // Create new element
+        const newElement = document.createElement(element);
+
+        // Give element content
+        const newContent = document.createTextNode(value);
+
+        // Add text node to element
+        newElement.appendChild(newContent);
+
+        // Add new element to target
+        target.appendChild(newElement);
+    };
+
+    // Show initial state
+    const updateScreen = () => {
+        // Clear game board content
+        gameBoardDiv.textContent = '';
+
+        // Get current state
+        const board = game.getBoard();
+        const activePlayer = game.getPlayer();
+
+        // Set player turn content
+        playerTurnDiv.textContent = activePlayer.name;
+
+        // Set game board content
+        for (let i = 0; i < board.length; i += 1) {
+            for (let j = 0; j < board[i].length; j += 1) {
+                const value = board[i][j].getValue();
+                addElement(gameBoardDiv, 'div', value);
+                console.log(value);
+            }
+        }
+    };
+    return { updateScreen };
 })();
